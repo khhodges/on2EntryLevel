e4c15b180a6d21d516c9263b07c80c1defd125e1
Ken Hh <sipantic@gmail.com>
2015-12-31 22:42:32 +0000

    Changes made by Ken Hh (sipantic@gmail.com).

--- my-friends-app/scripts/app/app.js
+++ my-friends-app/scripts/app/app.js
@@ -1,147 +1,212 @@
 var app = (function (win) {
-    'use strict';
+	'use strict';
 
-    // Global error handling
-    var showAlert = function(message, title, callback) {
-        navigator.notification.alert(message, callback || function () {
-        }, title, 'OK');
-    };
+	// Global error handling
+	var showAlert = function(message, title, callback) {
+		navigator.notification.alert(message, callback || function () {
+		}, title, 'OK');
+	};
 
-    var showError = function(message) {
-        showAlert(message, 'Error occured');
-    };
+	var showError = function(message) {
+		showAlert(message, 'Error occured');
+	};
+	
+	window.onerror = function (message, file, line) {
+		alert("Error: " + message + ", File: " + file + ", Line: " + line);
+	}
 
-    win.addEventListener('error', function (e) {
-        e.preventDefault();
+	win.addEventListener('error', function (e) {
+		e.preventDefault();
 
-        var message = e.message + "' from " + e.filename + ":" + e.lineno;
+		var message = e.message + "' from " + e.filename + ":" + e.lineno;
 
-        showAlert(message, 'Error occured');
+		showAlert(message, 'Error occured');
 
-        return true;
-    });
+		return true;
+	});
 
-    // Global confirm dialog
-    var showConfirm = function(message, title, callback) {
-        navigator.notification.confirm(message, callback || function () {
-        }, title, ['OK', 'Cancel']);
-    };
+	// Global confirm dialog
+	var showConfirm = function(message, title, callback) {
+		navigator.notification.confirm(message, callback || function () {
+		}, title, ['OK', 'Cancel']);
+	};
 
-    var isNullOrEmpty = function (value) {
-        return typeof value === 'undefined' || value === null || value === '';
-    };
+	var isNullOrEmpty = function (value) {
+		return typeof value === 'undefined' || value === null || value === '';
+	};
 
-    var isKeySet = function (key) {
-        var regEx = /^\$[A-Z_]+\$$/;
-        return !isNullOrEmpty(key) && !regEx.test(key);
-    };
+	var isKeySet = function (key) {
+		var regEx = /^\$[A-Z_]+\$$/;
+		return !isNullOrEmpty(key) && !regEx.test(key);
+	};
 
-    // Handle device back button tap
-    var onBackKeyDown = function(e) {
-        e.preventDefault();
+	// Handle device back button tap
+	var onBackKeyDown = function(e) {
+		e.preventDefault();
 
-        navigator.notification.confirm('Do you really want to exit?', function (confirmed) {
-            var exit = function () {
-                navigator.app.exitApp();
-            };
+		navigator.notification.confirm('Do you really want to exit?', function (confirmed) {
+			var exit = function () {
+				navigator.app.exitApp();
+			};
 
-            if (confirmed === true || confirmed === 1) {
-                // Stop EQATEC analytics monitor on app exit
-                if (analytics.isAnalytics()) {
-                    analytics.Stop();
-                }
-                AppHelper.logout().then(exit, exit);
-            }
-        }, 'Exit', ['OK', 'Cancel']);
-    };
+			if (confirmed === true || confirmed === 1) {
+				// Stop EQATEC analytics monitor on app exit
+				if (analytics.isAnalytics()) {
+					analytics.Stop();
+				}
+				AppHelper.logout().then(exit, exit);
+			}
+		}, 'Exit', ['OK', 'Cancel']);
+	};
 
-    var onDeviceReady = function() {
-        // Handle "backbutton" event
-        document.addEventListener('backbutton', onBackKeyDown, false);
+	var onDeviceReady = function() {
+		// Handle "backbutton" event
+		document.addEventListener('backbutton', onBackKeyDown, false);
 
-        navigator.splashscreen.hide();
+/*		document.addEventListener('deviceready', function () {
+			feedback.initialize('wewpzbnzheaxgay7');
+		});*/
 
-        if (analytics.isAnalytics()) {
-            analytics.Start();
-        }
+		navigator.splashscreen.hide();
+
+		if (analytics.isAnalytics()) {
+			analytics.Start();
+		}
         
-        // Initialize AppFeedback
-        if (app.isKeySet(appSettings.feedback.apiKey)) {
-            try {
-                feedback.initialize(appSettings.feedback.apiKey);
-            } catch (err) {
-                console.log('Something went wrong:');
-                console.log(err);
-            }
-        } else {
-            console.log('Telerik AppFeedback API key is not set. You cannot use feedback service.');
-        }
-    };
+		// Initialize AppFeedback
+		if (app.isKeySet(appSettings.feedback.apiKey)) {
+			try {
+				feedback.initialize(appSettings.feedback.apiKey);
+			} catch (err) {
+				console.log('Something went wrong:');
+				console.log(err);
+			}
+		} else {
+			console.log('Telerik AppFeedback API key is not set. You cannot use feedback service.');
+		}
+	};
 
-    // Handle "deviceready" event
-    document.addEventListener('deviceready', onDeviceReady, false);
+	// Handle "deviceready" event
+	document.addEventListener('deviceready', onDeviceReady, false);
  
-    // Initialize Everlive SDK
-    var el = new Everlive({
-                              appId: appSettings.everlive.appId,
-                              scheme: appSettings.everlive.scheme
-                          });
+	// Initialize Everlive SDK
+	var el = new Everlive({
+							  appId: appSettings.everlive.appId,
+							  scheme: appSettings.everlive.scheme
+						  });
 
-    var emptyGuid = '00000000-0000-0000-0000-000000000000';
+	var emptyGuid = '00000000-0000-0000-0000-000000000000';
 
-    var AppHelper = {
+	var AppHelper = {
+		
+		// Return absolute user profile picture url
+		resolveBackgroundPictureUrl: function (id) {
+			if (id && id !== emptyGuid) {
+				return 'url(' + el.Files.getDownloadUrl(id) + ')';
+			} else {
+				return 'styles/images/avatar.png';
+			}
+		},
 
-        // Return user profile picture url
-        resolveProfilePictureUrl: function (id) {
-            if (id && id !== emptyGuid) {
-                return el.Files.getDownloadUrl(id);
-            } else {
-                return 'styles/images/avatar.png';
-            }
-        },
+		// Return user profile picture url
+		resolveProfilePictureUrl: function (id) {
+			if (id && id !== emptyGuid) {
+				return el.Files.getDownloadUrl(id);
+			} else {
+				return 'styles/images/avatar.png';
+			}
+		},
 
-        // Return current activity picture url
-        resolvePictureUrl: function (id) {
-            if (id && id !== emptyGuid) {
-                return el.Files.getDownloadUrl(id);
-            } else {
-                return '';
-            }
-        },
+		// Return current activity picture url
+		resolvePictureUrl: function (id) {
+			if (id && id !== emptyGuid) {
+				return el.Files.getDownloadUrl(id);
+			} else {
+				return '';
+			}
+		},
 
-        // Date formatter. Return date in d.m.yyyy format
-        formatDate: function (dateString) {
-            return kendo.toString(new Date(dateString), 'MMM d, yyyy');
-        },
+		// Date formatter. Return date in d.m.yyyy format
+		formatDate: function (dateString) {
+			return kendo.toString(new Date(dateString), 'MMM d, yyyy');
+		},
 
-        // Current user logout
-        logout: function () {
-            return el.Users.logout();
-        },
+		// Current user logout
+		logout: function () {
+			return el.Users.logout();
+		},
         
-        autoSizeTextarea: function () {
-            var rows = $(this).val().split('\n');
-            $(this).prop('rows', rows.length + 1);
-        }
-    };
+		autoSizeTextarea: function () {
+			var rows = $(this).val().split('\n');
+			$(this).prop('rows', rows.length + 1);
+		},
+		
+		convertToDataURL: function convertToDataURLviaCanvas(url, callback, outputFormat) {
+			var img = new Image();
+			img.crossOrigin = 'Anonymous';
+			img.onload = function() {
+				var canvas = document.createElement('CANVAS');
+				var ctx = canvas.getContext('2d');
+				var dataURL;
+				canvas.height = this.height;
+				canvas.width = this.width;
+				ctx.drawImage(this, 0, 0);
+				dataURL = canvas.toDataURL(outputFormat, 0.5);
+				var ImgData = dataURL.substring("data:image/jepg;base64,".length);
+				callback(ImgData);
+				canvas = null; 
+			};
+			img.src = url;
+		}
+	};
+	
+	var fileHelper = {
+		
+		uploadPhoto: function (imageURI, server) {
+			var options = new FileUploadOptions();
+			options.fileKey = "file";
+			options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
+			options.mimeType = "image/jpeg";
 
-    var os = kendo.support.mobileOS,
-        statusBarStyle = os.ios && os.flatVersion >= 700 ? 'black-translucent' : 'black';
+			var params = {};
+			params.value1 = "test";
+			params.value2 = "param";
 
-    // Initialize KendoUI mobile application
-    var mobileApp = new kendo.mobile.Application(document.body, {
-                                                     transition: 'slide',
-                                                     statusBarStyle: statusBarStyle,
-                                                     skin: 'flat'
-                                                 });
+			options.params = params;
 
-    return {
-        showAlert: showAlert,
-        showError: showError,
-        showConfirm: showConfirm,
-        isKeySet: isKeySet,
-        mobileApp: mobileApp,
-        helper: AppHelper,
-        everlive: el
-    };
+			var ft = new FileTransfer();
+			ft.upload(imageURI, encodeURI(server), win, fail, options);
+		},
+
+		win: function (r) {
+			console.log("Code = " + r.responseCode);
+			console.log("Response = " + r.response);
+			console.log("Sent = " + r.bytesSent);
+		},
+		fail:function (error) {
+			alert("An error has occurred: Code = " + error.code);
+			console.log("upload error source " + error.source);
+			console.log("upload error target " + error.target);
+		}
+	}
+	
+	var os = kendo.support.mobileOS,
+		statusBarStyle = os.ios && os.flatVersion >= 700 ? 'black-translucent' : 'black';
+
+	// Initialize KendoUI mobile application
+	var mobileApp = new kendo.mobile.Application(document.body, {
+													 transition: 'slide',
+													 statusBarStyle: statusBarStyle,
+													 skin: 'flat'
+												 });
+
+	return {
+		showAlert: showAlert,
+		showError: showError,
+		showConfirm: showConfirm,
+		isKeySet: isKeySet,
+		mobileApp: mobileApp,
+		helper: AppHelper,
+		everlive: el
+	};
 }(window));
--- my-friends-app/scripts/app/signup.js
+++ my-friends-app/scripts/app/signup.js
@@ -1,104 +1,239 @@
 /**
  * Signup view model
  */
-var app = app || {};
+var app = app || {}; // reuse the existing app or create a new, empty app container
 
 app.Signup = (function () {
-    'use strict';
+	'use strict'; //This JavaScript 1.8.5 (ECMAScript version 5) is a literal expression, 
+	//ignored by earlier versions to throw silent errors, fixes mistakes and add optimizations: 
+	//strict mode code may run faster and prevents syntax needed for later versions of ECMAScript.
 
-    var singupViewModel = (function () {
+	var singupViewModel = (function () {
+		// View model components see HTML ids
+		var $signUpForm;
+		var $formFields;
+		var $signupInfo;
+		var $signupBtnWrp;
 
-        var dataSource;
-        var $signUpForm;
-        var $formFields;
-        var $signupInfo;
-        var $signupBtnWrp;
-        var validator;
+		// functional components
+		var dataSource;
+		var validator;
+		var avatarImage;
+		var touchzone;
+		var imageObj;
+		var lastMove;
+		var selected = "styles/images/avator.png";
 
-        // Register user after required fields (username and password) are validated in Backend Services
-        var signup = function () {
+		// Register user after required fields (username and password) are validated in Backend Services
+		var signup = function () {
+			dataSource.Gender = parseInt(dataSource.Gender);
+			var birthDate = new Date(dataSource.BirthDate);
 
-            dataSource.Gender = parseInt(dataSource.Gender);
-            var birthDate = new Date(dataSource.BirthDate);
+			if (birthDate.toJSON() === null) {
+				birthDate = new Date();
+			}
+			
+			dataSource.BirthDate = birthDate;
+			dataSource.Picture = "204f9c00-a9ae-11e5-bccb-8db8d990cdfd";
 
-            if (birthDate.toJSON() === null) {
-                birthDate = new Date();
-            }
+			Everlive.$.Users.register(
+					dataSource.Username,
+					dataSource.Password,
+					dataSource)
+				.then(function () {
+						app.showAlert("Congratulations! You are now registered!", "The Loyalty Club");
+						app.mobileApp.navigate('#welcome');
+					},
+					function (err) {
+						app.showError(err.message);
+					});
+		};
 
-            dataSource.BirthDate = birthDate;
+		// Executed after Signup view initialization
+		// init form validator
+		var init = function () {
+			// Get a reference to our touch-sensitive element
+			touchzone = document.getElementById("touchzone");
+			avatarImage = document.getElementById("avatarImage");
+			imageObj = new Image();
 
-            Everlive.$.Users.register(
-                dataSource.Username,
-                dataSource.Password,
-                dataSource)
-            .then(function () {
-                app.showAlert("Registration successful");
-                app.mobileApp.navigate('#welcome');
-            },
-            function (err) {
-                app.showError(err.message);
-            });
-        };
+			$signUpForm = $('#signUp');
+			$formFields = $signUpForm.find('input, textarea, select');
+			$signupInfo = $('#signupInfo');
+			$signupBtnWrp = $('#signupBtnWrp');
+			validator = $signUpForm.kendoValidator({
+				validateOnBlur: false
+			}).data('kendoValidator');
 
-        // Executed after Signup view initialization
-        // init form validator
-        var init = function () {
+			$formFields.on('keyup keypress blur change input', function () {
+				if (validator.validate()) {
+					$signupBtnWrp.removeClass('disabled');
+				} else {
+					$signupBtnWrp.addClass('disabled');
+				}
+			});
 
-            $signUpForm = $('#signUp');
-            $formFields = $signUpForm.find('input, textarea, select');
-            $signupInfo = $('#signupInfo');
-            $signupBtnWrp = $('#signupBtnWrp');
-            validator = $signUpForm.kendoValidator({ validateOnBlur: false }).data('kendoValidator');
+			$signupInfo.on('keydown', app.helper.autoSizeTextarea);
+		}
 
-            $formFields.on('keyup keypress blur change input', function () {
-                if (validator.validate()) {
-                    $signupBtnWrp.removeClass('disabled');
-                } else {
-                    $signupBtnWrp.addClass('disabled');
-                }
-            });
+		// Executed after show of the Signup view
+		var show = function () {
+			$signupInfo.prop('rows', 1);
+			dataSource = kendo.observable({
+				Username: '',
+				Password: '',
+				DisplayName: '',
+				Email: '',
+				Gender: '0',
+				About: 'styles/images/avatar.png',
+				Friends: [],
+				BirthDate: new Date()
+			});
+			kendo.bind($('#signup-form'), dataSource, kendo.mobile.ui);
+		};
 
-            $signupInfo.on('keydown', app.helper.autoSizeTextarea);
-        }
+		// Executed after hide of the Signup view
+		// disable signup button
+		var hide = function () {
+			$signupBtnWrp.addClass('disabled');
+		};
 
-        // Executed after show of the Signup view
-        var show = function () {
-            $signupInfo.prop('rows', 1);
+		var onSelectChange = function (sel) {
+			var selected = sel.options[sel.selectedIndex].value;
+			sel.style.color = (selected === 0) ? '#b6c5c6' : '#34495e';
+		};
 
-            dataSource = kendo.observable({
-                Username: '',
-                Password: '',
-                DisplayName: '',
-                Email: '',
-                Gender: '0',
-                About: '',
-                Friends: [],
-                BirthDate: new Date()
-            });
-            kendo.bind($('#signup-form'), dataSource, kendo.mobile.ui);
-        };
+		var everlive = new Everlive("wewpzbnzheaxgay7");
+		// Called when a photo is successfully retrieved
+		//
+		var resetImage = function () {
+			var image = document.getElementById("zoom_mw");
+			image.src = selected;
+		}
+		var pickImage = function () {
+			function success(imageURI) {
+				selected = imageURI;
+				avatarImage.style.backgroundImage = "url(" + selected + ")";
+				app.mobileApp.hideLoading();
+			}
 
-        // Executed after hide of the Signup view
-        // disable signup button
-        var hide = function () {
-            $signupBtnWrp.addClass('disabled');
-        };
+			var error = function () {
+				app.mobileApp.hideLoading();
+				navigator.notification.alert("No selection was detected.");
+			};
+			var config = {
+				destinationType: Camera.DestinationType.FILE_URI,
+				quality: 50
+			};
+			navigator.camera.getPicture(success, error, config);
+		};
 
-        var onSelectChange = function (sel) {
-            var selected = sel.options[sel.selectedIndex].value;
-            sel.style.color = (selected === 0) ? '#b6c5c6' : '#34495e';
-        }
+		var addImage = function () {
+			var success = function (data) {
+				everlive.Files.create({
+						Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
+						ContentType: "image/jpeg",
+						base64: data
+					})
+					.then(function (promise) {
+						selected = promise.result.Uri;
+						avatarImage.style.backgroundImage = "url(" + selected + ")";
+						app.mobileApp.hideLoading();
+					})
+			};
+			var error = function () {
+				app.mobileApp.hideLoading();
+				navigator.notification.alert("Unfortunately we could not add the image");
+			};
+			var config = {
+				destinationType: Camera.DestinationType.DATA_URL,
+				quality: 50
+			};
+			app.mobileApp.showLoading();
+			navigator.camera.getPicture(success, error, config);
+		};
 
-        return {
-            init: init,
-            show: show,
-            hide: hide,
-            onSelectChange: onSelectChange,
-            signup: signup
-        };
+		var loadPhotos = function (promise) {
+			dataSource.About = promise.result.Uri;
+			//app.showAlert("addImage to " + dataSource.About);
+			var files = [];
+			if (promise.result.Uri === "") {
+				everlive.Files.get().then(function (data) {
+					for (var i = data.result.length - 1; i >= 0; i--) {
+						var image = data.result[i];
+						files.push(image.Uri);
+						$("#images").kendoMobileListView({
+							dataSource: files,
+							template: "<ui><div class='adiv' ><img class='crop' src='#: data #' /></ui>"
+						});
+					}
+				})
+			} else {
+				var largeImage = document.getElementById('largeImage');
+				largeImage.style.display = 'block';
+				largeImage.src = promise.result.Uri;
+			}
+			app.mobileApp.hideLoading();
+		};
 
-    }());
+		var cropAvatar = function (X, Y) {
+			var source = new Image();
+			var d = document.getElementById("zoom_mw");
+			source.src = d.src;
+			var canvas = document.getElementById("myCanvas");
+			var avatar = document.getElementById("myCanvas");
+			var context = canvas.getContext("2d");
+			canvas.width = source.width;
+			canvas.height = source.height;
+			if (source.height > source.width) {
+				context.drawImage(source, X, Y, source.height / 2, source.height / 2, 0, 0, source.height, source.height);
+				avatar.style.height = source.height / 2;
+			} else {
+				context.drawImage(source, X, Y, source.width / 2, source.width / 2, 0, 0, source.width, source.width);
+				avatar.style.width = source.width / 2;
+			}
+			var size = 200;
+			var scale = 10;
+			context.font = size + "px impact";
+			var lineOfText = "Kenneth Hamer-Hodges";
 
-    return singupViewModel;
+			while (context.measureText(lineOfText).width / canvas.offsetWidth > 1) {
+				size = size - scale;
+				context.font = size + "px impact";
+				if (size < 30)
+					scale = 2;
+			}
+			context.textAlign = 'center';
+			context.fillStyle = 'yellow';
 
+			context.fillText(lineOfText, canvas.width / 2, canvas.height * 0.6);
+
+			var imgURI = canvas.toDataURL();
+
+			setTimeout(function () {
+				var imageA = document.getElementById("avatarImage");
+				imageA.src = imgURI;
+				if (imageA.style.height > imageA.style.width) {
+					imageA.style.height = "100%";
+					imageA.style.width = "auto";
+				} else {
+					imageA.style.height = "auto";
+					imageA.style.width = "100%";
+				}
+				canvas.style.visibility = 'hidden';
+			}, 200);
+		}
+		return {
+			init: init,
+			show: show,
+			hide: hide,
+			onSelectChange: onSelectChange,
+			signup: signup,
+			addImage: addImage,
+			pickImage: pickImage,
+			resetImage: resetImage
+		};
+	}());
+
+	return singupViewModel;
 }());
