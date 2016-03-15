d178817fd3e0333f5e24dd0f39ee00500e0a2cc4
Ken Hh <sipantic@gmail.com>
2016-03-15 12:32:23 +0000

    Changes made by Ken Hh (sipantic@gmail.com).

--- sharing/scripts/app/activities.js
+++ sharing/scripts/app/activities.js
@@ -8,20 +8,20 @@ var app = app || {};
 app.Activities = (function () {
 	'use strict'
 	var $enterEvent, $newEventText, validator, selected, $baseImage;
-
+	
 	var init = function () {
 		validator = $('#enterEvent').kendoValidator().data('kendoValidator');
-		$enterEvent = $('#enterEvent');
+		$enterEvent = $('#enterEvent');	
 		$newEventText = $('#newEventText');
 		$newEventText.on('keydown', app.helper.autoSizeTextarea);
 		validator.hideMessages();
 	};
-
-	var showTitle = function () {
+	
+	var showTitle = function() {
 		var title = document.getElementById("navbarTitle").InnerTEXT;
 		title = activities.User().DisplayName;
 	}
-
+	
 	// Activities model
 	var activitiesModel = (function () {
 		var activityModel = {
@@ -29,34 +29,34 @@ app.Activities = (function () {
 			id: 'Id',
 			fields: {
 				Text: {
-					field: 'Text',
-					defaultValue: ''
-				},
+						field: 'Text',
+						defaultValue: ''
+					},
 				CreatedAt: {
-					field: 'CreatedAt',
-					defaultValue: new Date()
-				},
+						field: 'CreatedAt',
+						defaultValue: new Date()
+					},
 				Picture: {
-					fields: 'Picture',
-					defaultValue: null
-				},
+						fields: 'Picture',
+						defaultValue: null
+					},
 				UserId: {
-					field: 'UserId',
-					defaultValue: null
-				},
+						field: 'UserId',
+						defaultValue: null
+					},
 				Likes: {
-					field: 'Likes',
-					defaultValue: []
-				}
+						field: 'Likes',
+						defaultValue: []
+					}
 			},
 			CreatedAtFormatted: function () {
 				return app.helper.formatDate(this.get('CreatedAt'));
 			},
-
+			
 			LikesCount: function () {
 				return app.helper.formatLikes(this.get('Likes'), this.get('Text'));
 			},
-
+			
 			PictureUrl: function () {
 				return app.helper.resolvePictureUrl(this.get('Picture'));
 			},
@@ -64,25 +64,26 @@ app.Activities = (function () {
 			var result = app.helper.ResponsiveImageUrl(this.get('Picture'));
 			return result;
 			},*/
-
+			
 			ResponsivePictureUrl: function () {
 				var id = this.get('Picture');
 				var el = new Everlive(appSettings.everlive.appId);
-				el.Files.getById(id).then(function (data) {
-						// get url from data
-						var url = data.result.Uri;
-						var size = "/resize=w:200,h:200,fill:cover/";
-						var base = "https://bs1.cdn.telerik.com/image/v1/";
-						//navigator.notification.alert(url);
-						// convert to responsive url
-						url = base + appSettings.everlive.appId + size + url;
-						return url;
-					}),
-					function (error) {
-						navigator.notification.alert(JSON.stringify(error));
-					}
+				el.Files.getById(id).then
+				(function(data) {
+					// get url from data
+					var url = data.result.Uri;
+					var size = "/resize=w:200,h:200,fill:cover/";
+					var base = "https://bs1.cdn.telerik.com/image/v1/";
+					//navigator.notification.alert(url);
+					// convert to responsive url
+					url = base + appSettings.everlive.appId + size + url;
+					return url;
+				}),
+				function(error) {
+					navigator.notification.alert(JSON.stringify(error));
+				}
 			},
-
+			
 			User: function () {
 				var userId = this.get('UserId');
 				if (userId === undefined) {
@@ -97,11 +98,11 @@ app.Activities = (function () {
 				return user ? {
 					DisplayName: user.DisplayName,
 					PictureUrl: app.helper.resolveProfilePictureUrl(user.Picture),
-					urlPictureUrl: app.helper.resolveBackgroundPictureUrl(user.Picture, 'bg')
+					urlPictureUrl: app.helper.resolveBackgroundPictureUrl(user.Picture,'bg')
 				} : {
 					DisplayName: app.Users.currentUser.data.DisplayName,
 					PictureUrl: app.helper.resolveProfilePictureUrl(app.Users.currentUser.data.Picture),
-					urlPictureUrl: app.helper.resolveBackgroundPictureUrl(app.Users.currentUser.data.Picture, 'bg')
+					urlPictureUrl: app.helper.resolveBackgroundPictureUrl(app.Users.currentUser.data.Picture,'bg')
 				};
 			},
 			isVisible: function () {
@@ -114,31 +115,29 @@ app.Activities = (function () {
 		// Activities data source. The Backend Services dialect of the Kendo UI DataSource component
 		// supports filtering, sorting, paging, and CRUD operations.
 		var activitiesDataSource = new kendo.data.DataSource({
-			type: 'everlive',
-			schema: {
+																 type: 'everlive',
+																 schema: {
 				model: activityModel
 			},
-			transport: {
+																 transport: {
 				// Required by Backend Services
 				typeName: 'Activities'
 			},
-			change: function (e) {
-				if (e.items && e.items.length > 0) {
-					$('#no-activities-span').hide();
-				} else {
-					$('#no-activities-span').show();
-				}
-			},
-			sort: {
+																 change: function (e) {
+																	 if (e.items && e.items.length > 0) {
+																		 $('#no-activities-span').hide();
+																	 } else {
+																		 $('#no-activities-span').show();
+																	 }
+																 },
+																 sort: {
 				field: 'CreatedAt',
 				dir: 'desc'
 			},
-			filter: {
-				field: "UserId",
-				operator: "neq",
-				value: "undefined"
+																 filter:{
+				field: "UserId", operator: "neq", value: "undefined"				
 			}
-		});
+															 });
 
 		return {
 			activities: activitiesDataSource
@@ -147,81 +146,82 @@ app.Activities = (function () {
 
 	// Activities view model
 	var activitiesViewModel = (function () {
-			// Navigate to activityView When some activity is selected
-			var activitySelected = function (e) {
-				app.mobileApp.navigate('views/activityView.html?uid=' + e.data.uid);
-			};
+		// Navigate to activityView When some activity is selected
+		var activitySelected = function (e) {
+			app.mobileApp.navigate('views/activityView.html?uid=' + e.data.uid);
+		};
 
-			// Navigate to app home
-			var navigateHome = function () {
-				app.mobileApp.navigate('#welcome');
-			};
+		// Navigate to app home
+		var navigateHome = function () {
+			app.mobileApp.navigate('#welcome');
+		};
 
-			// Logout user
-			var logout = function () {
-				app.helper.logout()
-					.then(navigateHome, function (err) {
-						app.showError(err.message);
-						navigateHome();
-					});
-			};
+		// Logout user
+		var logout = function () {
+			app.helper.logout()
+				.then(navigateHome, function (err) {
+					app.showError(err.message);
+					navigateHome();
+				});
+		};
 
-			var crop = function () {
-				var sx, sy, starterWidth, starterHeight, dx, dy, canvasWidth, canvasHeight;
-				var starter = document.getElementById("picture");
-				var canvas = document.getElementById("canvas");
-				if (starter.naturalWidth > starter.naturalHeight) {
-					sx = (starter.naturalWidth - starter.naturalHeight) / 2;
-					starterWidth = starter.naturalHeight;
-					starterHeight = starter.naturalHeight;
-					sy = 0;
-				} else {
-					sy = (-starter.naturalWidth + starter.naturalHeight) / 2;
-					starterWidth = starter.naturalWidth;
-					starterHeight = starter.naturalWidth;
-					sx = 0;
-				}
-				dx = 0;
-				dy = 0;
-				canvasWidth = canvas.width;
-				canvasHeight = canvas.height;
-				var ctx = canvas.getContext("2d");
-				ctx.drawImage(starter, sx, sy, starterWidth, starterHeight, dx, dy, canvasWidth, canvasHeight);
-				$baseImage = canvas.toDataURL("image/jpeg", 1.0).substring("data:image/jpeg;base64,".length);
+		var crop = function () {
+			var sx, sy, starterWidth, starterHeight, dx, dy, canvasWidth, canvasHeight;
+			var starter = document.getElementById("picture");
+			var canvas = document.getElementById("canvas");
+			if (starter.naturalWidth > starter.naturalHeight) {
+				sx = (starter.naturalWidth - starter.naturalHeight) / 2;
+				starterWidth = starter.naturalHeight;
+				starterHeight = starter.naturalHeight;
+				sy = 0;
+			} else {
+				sy = (- starter.naturalWidth + starter.naturalHeight) / 2;
+				starterWidth = starter.naturalWidth;
+				starterHeight = starter.naturalWidth;
+				sx = 0;
 			}
-			var saveActivity = function () {
-				// Validating of the required fields
-				if (validator.validate()) {
-					$enterEvent.style.display = 'none';
-					// Adding new comment to Comments model
-					var activities = app.Activities.activities;
-					var activity = activities.add();
-					activity.Text = $newEventText.val();
-					$newEventText.Val = "";
-					activity.UserId = app.Users.currentUser.get('data').Id;
-					document.getElementById('addButton').innerText = "Add Event";
-					app.mobileApp.showLoading();
-					activities.sync();
-					app.mobileApp.hideLoading();
+			dx = 0;
+			dy = 0;
+			canvasWidth = canvas.width;
+			canvasHeight = canvas.height;
+			var ctx = canvas.getContext("2d");
+			ctx.drawImage(starter, sx, sy, starterWidth, starterHeight, dx, dy, canvasWidth, canvasHeight);
+			$baseImage = canvas.toDataURL("image/jpeg", 1.0).substring("data:image/jpeg;base64,".length);
+		}
+		var saveActivity = function () {
+			// Validating of the required fields
+			if (validator.validate()) {
+				$enterEvent.style.display = 'none';				
+				// Adding new comment to Comments model
+				var activities = app.Activities.activities;
+				var activity = activities.add();                
+				activity.Text = $newEventText.val();
+				$newEventText.Val = "";
+				activity.UserId = app.Users.currentUser.get('data').Id; 				
+				document.getElementById('addButton').innerText = "Add Event";
+				app.mobileApp.showLoading();
+				activities.sync();
+				app.mobileApp.hideLoading();
+			}
+		};
+		
+		var saveImageActivity = function () {
+			// Validating of the required fields
+			if (selected === undefined || !$baseImage) {
+				app.showAlert("First take a photo with your camera and then add a message to match!", "Informational");
+			}
+			if (validator.validate() && (selected !== undefined)) {				
+				if (!app.helper.checkSimulator) {
+					window.plugins.toast.showShortTop("Uploading image ...")
 				}
-			};
-
-			var saveImageActivity = function () {
-				// Validating of the required fields
-				if (selected === undefined || !$baseImage) {
-					app.showAlert("First take a photo with your camera and then add a message to match!", "Informational");
-				}
-				if (validator.validate() && (selected !== undefined)) {
-					app.NotifyHelper.showShortTop("Uploading image ...");
-				}
-
+				;
 				app.mobileApp.showLoading();
 				// Save image as base64 to everlive
 				app.everlive.Files.create({
-						Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
-						ContentType: "image/jpeg",
-						base64: $baseImage
-					})
+											  Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
+											  ContentType: "image/jpeg",
+											  base64: $baseImage
+										  })
 					.then(function (promise) {
 						selected = promise.result.Id;
 
@@ -231,7 +231,7 @@ app.Activities = (function () {
 						activity.Text = document.getElementById('newEventText').value;
 						activity.UserId = app.Users.currentUser.get('data').Id;
 						activity.Picture = selected;
-						activities.sync();
+						activities.sync();						
 						$enterEvent.style.display = 'none';
 						validator.hideMessages();
 						document.getElementById('addButton').innerText = "Add Event";
@@ -254,7 +254,7 @@ app.Activities = (function () {
 				document.getElementById('addButton').innerText = "Cancel";
 			}
 		};
-
+		
 		var success = function (imageURI) {
 			selected = imageURI;
 			var picture = document.getElementById("picture");
@@ -262,13 +262,13 @@ app.Activities = (function () {
 		}
 
 		var error = function () {
-			app.showError("No selection was detected.");
+			app.showError("No selection was detected.");			
 			$enterEvent.style.display = 'none';
 			validator.hideMessages();
 			document.getElementById('addButton').innerText = "Add Event";
 			document.getElementById('newEventText').value = "";
 		};
-
+		
 		var pickImage = function (e) {
 			$enterEvent = document.getElementById('enterEvent');
 			app.mobileApp.navigate('#view-all-activities');
@@ -282,16 +282,16 @@ app.Activities = (function () {
 				$enterEvent.style.display = 'block';
 				document.getElementById('addButton').innerText = "Cancel";
 				navigator.camera.getPicture(success, error, {
-					//kjhh best result including iphone rotation
-					quality: 100,
-					destinationType: navigator.camera.DestinationType.FILE_URI,
-					sourceType: navigator.camera.PictureSourceType.CAMERA,
-					encodingType: navigator.camera.EncodingType.JPEG,
-					correctOrientation: true
-				});
+												//kjhh best result including iphone rotation
+												quality: 100, 
+												destinationType: navigator.camera.DestinationType.FILE_URI,
+												sourceType: navigator.camera.PictureSourceType.CAMERA,
+												encodingType: navigator.camera.EncodingType.JPEG,
+												correctOrientation: true
+											});
 			}
 		};
-
+		
 		return {
 			init: init,
 			activities: activitiesModel.activities,
@@ -304,5 +304,5 @@ app.Activities = (function () {
 		};
 	}());
 
-return activitiesViewModel;
+	return activitiesViewModel;
 }());
